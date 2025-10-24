7e434a83147b8bf902576136e463e03b640708eb
Dineshs737 <dinesh77saarck@gmail.com>
2025-10-24 08:56:32 +0530

    refactor code

---
+++ src/interface/GitHubStats.interface.ts
@@ -0,0 +1,17 @@
+export interface GitHubStats {
+  name: string;
+  username: string;
+  location: string;
+  bio: string;
+  company: string;
+  blog: string;
+  repositories: number;
+  followers: number;
+  following: number;
+  commits: number;
+  pullRequests: number;
+  issues: number;
+  stars: number;
+  streak: number;
+  linesOfCode: number;
+}
